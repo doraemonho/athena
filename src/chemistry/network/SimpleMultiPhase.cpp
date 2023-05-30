f9c35d99f0700e94c53e9ad2dfdc9b624a427df4
woodyho13@gmail.com <HOphy!19>
2023-05-29 21:48:11 -0600

    Update for T_floor in SimpleMultiPhase.pgen

--- src/chemistry/network/SimpleMultiPhase.cpp
+++ src/chemistry/network/SimpleMultiPhase.cpp
@@ -103,7 +103,7 @@ Real ChemNetwork::Edot(const Real t, const Real y[NSCALARS], const Real ED) {
   Real Heating = 2e-26;
   Real Cooling = 2e-26*nH_*(1e7*exp(-1.184e5/(T+ 1e3)) + 1.4e-2*sqrt(T)*exp(-92/T));
   Real dEdt = Heating;
-  if (T > 50) {
+  if (T > 20.0) {
     dEdt = dEdt - Cooling;
   }
 
