d33dd25facb07db24f6b4ee46cf98451bbe97dc9
Ka Wai HO <woodyho13@gmail.com>
2023-07-17 19:17:45 -0600

    Added Gamma definition

--- src/task_list/time_integrator.cpp
+++ src/task_list/time_integrator.cpp
@@ -1571,7 +1571,7 @@ TaskStatus TimeIntegratorTaskList::ReceiveAndCorrectEMF(MeshBlock *pmb, int stag
 TaskStatus TimeIntegratorTaskList::IntegrateHydro(MeshBlock *pmb, int stage) {
   Hydro *ph = pmb->phydro;
   Field *pf = pmb->pfield;
-
+  Real g = pmb->peos->GetGamma();
   if (pmb->pmy_mesh->fluid_setup != FluidFormulation::evolve) return TaskStatus::next;
 
   if (stage <= nstages) {
@@ -1594,7 +1594,7 @@ TaskStatus TimeIntegratorTaskList::IntegrateHydro(MeshBlock *pmb, int stage) {
         pmb->WeightedAve(ph->u, ph->u1, ph->u2, ph->u0, ph->fl_div, ave_wghts);
 
       const Real wght = stage_wghts[stage-1].beta*pmb->pmy_mesh->dt;
-      ph->AddFluxDivergence(wght, ph->u, pf->b, pf->bcc);
+      ph->AddFluxDivergence(wght, ph->u, pf->b, pf->bcc, g);
       // add coordinate (geometric) source terms
       pmb->pcoord->AddCoordTermsDivergence(wght, ph->flux, ph->w, pf->bcc, ph->u);
 
@@ -1609,7 +1609,7 @@ TaskStatus TimeIntegratorTaskList::IntegrateHydro(MeshBlock *pmb, int stage) {
         const Real wght_ssp = beta*pmb->pmy_mesh->dt;
         // writing out to u2 register
         pmb->WeightedAve(ph->u2, ph->u1, ph->u2, ph->u0, ph->fl_div, ave_wghts);
-        ph->AddFluxDivergence(wght_ssp, ph->u2, pf->b, pf->bcc);
+        ph->AddFluxDivergence(wght_ssp, ph->u2, pf->b, pf->bcc, g);
         // add coordinate (geometric) source terms
         pmb->pcoord->AddCoordTermsDivergence(wght_ssp, ph->flux, ph->w, pf->bcc, ph->u2);
       }
